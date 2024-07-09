c2e92ad325daf766d586d9b1879a079603979d60
Marone Taques <maronect@gmail.com>
2024-07-09 12:21:20 -0300

    feat: database, from pgadmin4 connected, .env configuration to connection added

--- first/src/main/java/com/maronecom/first/FirstApplication.java
+++ first/src/main/java/com/maronecom/first/FirstApplication.java
@@ -3,10 +3,13 @@ package com.maronecom.first;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class FirstApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
 		SpringApplication.run(FirstApplication.class, args);
 	}
 
